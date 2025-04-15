f527bd173406d7eb20e0d9b8e044eb4d399418ba
amo-hada <amirhdv83@gmail.com>
2025-04-15 15:41:18 +0330

    final commit

--- OldValueSample/NotifyPropertyChangedAttribute.cs
+++ OldValueSample/NotifyPropertyChangedAttribute.cs
@@ -1,10 +1,27 @@
-using System;
 using System.ComponentModel;
 using Metalama.Framework.Aspects;
 using Metalama.Framework.Code;
 
 internal class NotifyPropertyChangedAttribute : TypeAspect
 {
+    [Introduce]
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    [Introduce]
+    protected void OnPropertyChanged(string name)
+    {
+        this.PropertyChanged?.Invoke(meta.This, new PropertyChangedEventArgs(name));
+    }
+    
+    //This is the overload.
+    [Introduce]
+    protected void OnPropertyChanged(string name, object? oldValue, object? newValue)
+    {
+        this.PropertyChanged?.Invoke(meta.This, new PropertyChangedEventArgs(name));
+        
+        Console.WriteLine($"Property {name} changed from {oldValue} to {newValue}");
+    }
+    
     public override void BuildAspect(IAspectBuilder<INamedType> builder)
     {
         builder.Advice.ImplementInterface(
@@ -16,34 +33,26 @@ internal class NotifyPropertyChangedAttribute : TypeAspect
         {
             builder.Advice.OverrideAccessors(
                 property,
-                setTemplate: nameof(this.OverridePropertySetter));
+                setTemplate: nameof(OverridePropertySetter));
         }
     }
 
-    [InterfaceMember]
-    public event PropertyChangedEventHandler? PropertyChanged;
-
-    [Introduce]
-    protected void OnPropertyChanged(string name)
-    {
-        this.PropertyChanged?.Invoke(meta.This, new PropertyChangedEventArgs(name));
-    }
-
     [Template]
-    private dynamic OverridePropertySetter(dynamic value)
+    private void OverridePropertySetter(dynamic value)
     {
         var oldValue = meta.Target.Property.Value;
 
         if (!Equals(oldValue, value))
         {
             meta.Proceed();
+            
             this.OnPropertyChanged(meta.Target.Property.Name);
+            this.OnPropertyChanged(meta.Target.Property.Name, oldValue, value);
 
+      
             Console.WriteLine(
-                $"{meta.Target.Property.Name} has been updated to '{value}' and the old value was '{oldValue}'");
+                $"{meta.Target.Property.Name} updated to '{value}' (was '{oldValue}')");
         }
-
-        return value;
     }
+    
 }
-
--- OldValueSample/Product.cs
+++ OldValueSample/Product.cs
@@ -3,6 +3,6 @@
 [NotifyPropertyChanged]
 internal partial class Product
 {
-    public string Title { get; set; }
+    public string? Title { get; set; }
     public decimal Price { get; set; }
 }
